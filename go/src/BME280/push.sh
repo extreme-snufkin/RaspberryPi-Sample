6a2affc5019a74efab0855f59a1711f00bae80ef
extreme-snufkin <extreme-snufkin@gmail.com>
2018-01-08 02:38:07 +0900

    MultiPush対応

--- go/src/BME280/push.sh
+++ go/src/BME280/push.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 CURL=/usr/bin/curl
-PUSHBULLET_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 PUSHBULLET_TITLE=Raspberry\ Pi
 
 LANG=ja_JP.utf8
 
-${CURL} -u ${PUSHBULLET_TOKEN}: -X POST \
+${CURL} -u $1: -X POST \
     https://api.pushbullet.com/v2/pushes \
         --header "Content-Type: application/json" \
-            --data-binary "{\"type\": \"note\", \"title\": \"${PUSHBULLET_TITLE}\", \"body\": \"$1\"}"
+            --data-binary "{\"type\": \"note\", \"title\": \"${PUSHBULLET_TITLE}\", \"body\": \"$2\"}"
